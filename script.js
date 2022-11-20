7fd0b244c97c1ad575a0dea0b09768f75baab50f
Tehos <postolacheteodor@gmail.com>
2022-11-20 21:24:35 +0200

    Create calculator design

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// OPERATORS
 const add = function (a, b) {
   return a + b;
 };
@@ -17,3 +18,13 @@ const divide = function (a, b) {
 const operate = function (a, operator, b) {
   return operator(a, b);
 };
+
+// CREATE BUTTONS FOR DIGITS, OPERATORS AND EQUAL
+const digitButtons = document.querySelector(".digit-buttons");
+
+for (i = 0; i < 10; i++) {
+  const digitBtn = document.createElement("button");
+  digitBtn.classList.add(`digit-btn-${i}`);
+  digitBtn.textContent = [i];
+  digitButtons.appendChild(digitBtn);
+}
