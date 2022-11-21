50d55cd727d77f3d3a28aa04df11dd93cd6b2cc5
Tehos <postolacheteodor@gmail.com>
2022-11-21 13:19:28 +0200

    Start over

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 // OPERATORS
-const add = function (a, b) {
-  return a + b;
-};
-
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-const operate = function (a, operator, b) {
-  return operator(a, b);
-};
 
 // CREATE BUTTONS FOR DIGITS, OPERATORS AND EQUAL
 const digitButtons = document.querySelector(".digit-buttons");
@@ -33,21 +14,3 @@ for (i = 0; i < 10; i++) {
 const displayValue = document.querySelector(".display");
 const digitBtns = document.querySelectorAll(".digit-btn");
 displayValue.textContent = "";
-
-// const addValue = function () {
-//   displayValue.textContent += digit.textContent;
-// };
-
-let displayValueFull = false;
-
-digitBtns.forEach((digit) => {
-  digit.addEventListener("click", function addValue() {
-    if (displayValueFull === false) {
-      displayValue.textContent += digit.textContent;
-      console.log(displayValue.textContent.length);
-    }
-    displayValue.textContent.length > 10
-      ? (displayValueFull = true)
-      : (displayValueFull = false);
-  });
-});
