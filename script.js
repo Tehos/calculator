cda5b81b32a3825de0674070024b6e6268f5cbf5
Tehos <postolacheteodor@gmail.com>
2022-11-21 09:32:55 +0200

    Digit buttons display numbers

--- script.js
+++ script.js
@@ -23,8 +23,31 @@ const operate = function (a, operator, b) {
 const digitButtons = document.querySelector(".digit-buttons");
 
 for (i = 0; i < 10; i++) {
-  const digitBtn = document.createElement("button");
-  digitBtn.classList.add(`digit-btn-${i}`);
-  digitBtn.textContent = [i];
-  digitButtons.appendChild(digitBtn);
+  const digitButton = document.createElement("button");
+  digitButton.classList.add("digit-btn");
+  digitButton.textContent = [i];
+  digitButtons.appendChild(digitButton);
 }
+
+// DISPLAY VALUE
+const displayValue = document.querySelector(".display");
+const digitBtns = document.querySelectorAll(".digit-btn");
+displayValue.textContent = "";
+
+// const addValue = function () {
+//   displayValue.textContent += digit.textContent;
+// };
+
+let displayValueFull = false;
+
+digitBtns.forEach((digit) => {
+  digit.addEventListener("click", function addValue() {
+    if (displayValueFull === false) {
+      displayValue.textContent += digit.textContent;
+      console.log(displayValue.textContent.length);
+    }
+    displayValue.textContent.length > 10
+      ? (displayValueFull = true)
+      : (displayValueFull = false);
+  });
+});
